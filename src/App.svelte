0c768ad51b04dd535102acb8fb8ddc7b42547ce7
Amr Elaraby <amrselaraby@gmail.com>
2024-01-05 04:21:00 +0200

    finished intro to Svelete

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Counter from "./lib/Counter.svelte";
+
+  let props = {
+    initialCount: 5,
+    maxCount: 10,
+  };
+</script>
+
+<Counter {...props} x="y" />
+
+<style>
+</style>
